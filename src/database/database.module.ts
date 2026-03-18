4ff22fa2be1480c123f5eb58d56d9c8fe6d82556
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2026-03-18 09:59:32 +0530

    update: db logging based on env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,17 +10,21 @@ import { DatabaseLoggerService } from '@app/shared/app-logger/db-logger.service'
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService, DatabaseLoggerService],
-      useFactory: (configService: ConfigService, logger: DatabaseLoggerService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASS'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        logger,
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService, logger: DatabaseLoggerService) => {
+        const logging = configService.get<string>('DB_LOGGING') === 'true';
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_NAME'),
+          logging,
+          autoLoadEntities: true,
+          logger: logging ? logger : null,
+          synchronize: true,
+        };
+      },
     }),
   ],
 })
